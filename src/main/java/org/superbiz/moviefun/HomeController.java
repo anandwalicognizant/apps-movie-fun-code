07af7479ae27d516762b09c55196491cc0a10d13
Anand Waishetti <anand.walishetti@cognizant.com>
2017-12-13 10:34:57 -0600

    Updated for lab spring bootification

---
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -0,0 +1,42 @@
+package org.superbiz.moviefun;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by aw169 on 12/12/17.
+ */
+@Controller
+public class HomeController {
+
+    private MoviesBean moviesBean;
+
+    public HomeController(MoviesBean bean){
+        this.moviesBean = bean;
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+    @RequestMapping("/setup")
+    public String setup(Map<String, Object> model){
+        moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
+        moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
+        moviesBean.addMovie(new Movie("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
+        moviesBean.addMovie(new Movie("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
+        moviesBean.addMovie(new Movie("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
+        moviesBean.addMovie(new Movie("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
+        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
+        List<Movie> movies = moviesBean.getMovies();
+        model.put("movies", movies);
+        return "setup";
+    }
+}
